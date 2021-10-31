80d253696fc89c90c0b055551694fb30edec05d9
TheSpeedX <ggspeedx29@gmail.com>
2021-10-31 11:48:01 +0530

    make api key optional

--- models/verifier.py
+++ models/verifier.py
@@ -1,12 +1,13 @@
 from pydantic import BaseModel, Field
 from datetime import datetime
+from typing import Optional
 
 
 class Verifier(BaseModel):
     username: str = Field(max_length=64)
     password: str = Field(max_length=255)
     name: str = Field(max_length=255)
-    api_key: str = Field(max_length=255)
+    api_key: Optional[str] = Field(max_length=255)
     isActive: bool = True
     createdAt: datetime = datetime.utcnow()
     lastLogin: datetime = datetime.utcnow()
